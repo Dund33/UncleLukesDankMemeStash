d06e2aa719db72373fd6f812bccb1bbc9a854983
Dund33 <warior1112@gmail.com>
2020-09-20 18:40:34 +0200

    Finished localizing Meme

--- UncleLukesDankMemeStash/Models/Meme.cs
+++ UncleLukesDankMemeStash/Models/Meme.cs
@@ -1,37 +1,40 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.CodeAnalysis;
 using UncleLukesDankMemeStash.Areas.Identity;
+using UncleLukesDankMemeStash.Resources;
 
 namespace UncleLukesDankMemeStash.Models
 {
     public class Meme : IDisplayable
     {
-        [Display(Name = "Comment")]
+        [Display(Name = "Comment", ResourceType = typeof(MemeLocalization))]
         public string Comment { get; set; }
 
-        [Display(Name = "Category")]
+        [Display(Name = "Category", ResourceType = typeof(MemeLocalization))]
         [ForeignKey("Category")]
         public int CategoryID { get; set; }
 
-        [Display(Name = "Category")]
+        [Display(Name = "Category", ResourceType = typeof(MemeLocalization))]
         public Category Category { get; set; }
 
-        [Display(Name = "User")]
+        [Display(Name = "User", ResourceType = typeof(MemeLocalization))]
         [ForeignKey("MemeAuthor")]
         public string UserID { get; set; }
 
-        [Display(Name = "User")]
+        [Display(Name = "User", ResourceType = typeof(MemeLocalization))]
         public MemeAuthor User { get; set; }
 
         [Key]
         public int ID { get; set; }
 
         [Required(ErrorMessage = "Meme title is required")]
-        [Display(Name = "Title")]
+        [Display(Name = "Title", ResourceType = typeof(MemeLocalization))]
         public string Title { get; set; }
 
         [Required(ErrorMessage = "Image URL is required")]
         [Url]
+        [Display(Name = "URL")]
         public string ImageURL { get; set; }
     }
 }
